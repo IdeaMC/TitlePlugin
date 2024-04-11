7d92da19c07fd81f10b63fd88eb0eb3ca3014d59
suxiaolin <n652479618@outlook.com>
2024-04-11 12:19:31 +0800

    start

--- src/main/java/ideamc/titleplugin/TitlePlugin.java
+++ src/main/java/ideamc/titleplugin/TitlePlugin.java
@@ -1,17 +1,41 @@
 package ideamc.titleplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TitlePlugin extends JavaPlugin {
+import net.milkbowl.vault.economy.Economy;
+
+public class TitlePlugin extends JavaPlugin {
+
+    private static Economy econ = null;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        if (!setupEconomy() ) {
+            Bukkit.getConsoleSender().sendMessage("[TitlePlugin]§4前置Vault未找到已禁用本插件!");
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
     }
 }
--- src/main/java/ideamc/titleplugin/Vault/coin.java
+++ src/main/java/ideamc/titleplugin/Vault/coin.java
@@ -1,6 +1,23 @@
 package ideamc.titleplugin.Vault;
 
-import net.
+import ideamc.titleplugin.TitlePlugin;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class coin {
+    public boolean removecoin(String playername, int coin)
+    {
+        Economy economy = TitlePlugin.getEconomy();
+        EconomyResponse r = economy.withdrawPlayer(playername, coin);
+        Player player = Bukkit.getPlayer(playername);
+        if(r.transactionSuccess()) {
+            player.sendMessage(String.format("成功扣除金币 %s 剩余金币 %s", economy.format(r.amount), economy.format(r.balance)));
+            return true;
+        } else {
+            player.sendMessage(String.format("发生错误: %s", r.errorMessage));
+            return false;
+        }
+    }
 }
